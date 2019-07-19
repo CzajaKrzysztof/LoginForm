f63acc9cd3a7e90108778109ae142364a1e9ce94
Krzysiek <krzysiekczaja@gmail.com>
2019-07-19 18:43:06 +0200

    Add methods implementation for UserSQL class

--- src/main/java/com/codecool/krk/dao/IUserDao.java
+++ src/main/java/com/codecool/krk/dao/IUserDao.java
@@ -1,4 +1,7 @@
 package com.codecool.krk.dao;
 
+import com.codecool.krk.model.User;
+
 public interface IUserDao {
+    User getUserById(int userId);
 }
--- src/main/java/com/codecool/krk/dao/sql/UserSQL.java
+++ src/main/java/com/codecool/krk/dao/sql/UserSQL.java
@@ -1,11 +1,52 @@
 package com.codecool.krk.dao.sql;
 
 import com.codecool.krk.dao.IUserDao;
+import com.codecool.krk.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserSQL implements IUserDao {
     IConnectionPool connectionPool;
 
     public UserSQL(IConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
+    }
+
+    @Override
+    public User getUserById(int userId) {
+        String query = "SELECT * FROM users WHERE user_id = ?)";
+        User user = null;
+        try {
+            Connection connection = connectionPool.getConnection();
+            prepareSelectUser(userId, connection, query, user);
+            connectionPool.releaseConnection(connection);
+            return user;
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage()
+                    + "\nSQLState: " + e.getSQLState()
+                    + "\nVendorError: " + e.getErrorCode());
+        }
+        throw new RuntimeException("No user by that id");
+    }
+
+    private void prepareSelectUser(int userId, Connection connection, String query, User user) throws SQLException {
+        try(PreparedStatement stmt = connection.prepareStatement(query)) {
+            stmt.setInt(1, userId);
+            getUserData(stmt, user);
+        }
+    }
+
+    private void getUserData(PreparedStatement stmt, User user) throws SQLException {
+        try (ResultSet resultSet = stmt.executeQuery()) {
+            while (resultSet.next()) {
+                int userId = resultSet.getInt("user_id");
+                String name = resultSet.getString("name");
+                String type = resultSet.getString("type");
+                user = new User(userId, name, type);
+            }
+        }
     }
 }
